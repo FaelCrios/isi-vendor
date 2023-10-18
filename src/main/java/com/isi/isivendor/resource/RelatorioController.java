93c55087a6f19db78ef966305f33f823939d01ac
Rafael <faelnek@gmail.com>
2023-10-18 16:13:13 -0300

    Finish the email sender

--- src/main/java/com/isi/isivendor/resource/RelatorioController.java
+++ src/main/java/com/isi/isivendor/resource/RelatorioController.java
@@ -4,15 +4,20 @@ import com.isi.isivendor.entities.*;
 import com.isi.isivendor.service.EmailService;
 import com.isi.isivendor.service.PedidoService;
 import com.isi.isivendor.service.UsuarioService;
+import jakarta.validation.constraints.Email;
 import org.springframework.aot.hint.annotation.RegisterReflectionForBinding;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 
@@ -29,45 +34,70 @@ public class RelatorioController {
     @Autowired
     private UsuarioService userService;
 
+
     @GetMapping
-    public ResponseEntity<Void> sendRelatorio(@RequestBody String email) {
-        List<Pedido> pedidos = services.findAll();
-        String subject = "E-COMMERCE APPLICATION | Envio de Relatório";
+    public ResponseEntity<Void> sendRelatorio(@RequestBody Map<String, String> requestBody) {
+        try {
+            String email = requestBody.get("email"); // Extrai o valor do campo "email" do JSON
+            List<Pedido> pedidos = services.findAll();
+            String subject = "E-COMMERCE APPLICATION | Envio de Relatório";
 
-        // Construa uma mensagem personalizada
-        StringBuilder message = new StringBuilder("Lista de Pedidos:\n");
-        for (Pedido pedido : pedidos) {
-            message.append("-----------------------------------------------------").append("\n");
-            message.append("ID do Pedido: ").append(pedido.getId()).append("\n");
-            message.append("Data do Pedido: ").append(pedido.getInstante()).append("\n");
-            message.append("Status do pedido: ").append(pedido.getPedidoStatus()).append("\n"); // Status da compra
-            message.append("Id do usuario: ").append(pedido.getUsuario().getId()).append("\n");;
-            message.append("Nome do Usuário: ").append(pedido.getUsuario().getNome()).append(" ").append(pedido.getUsuario().getSobrenome()).append("\n"); // Obtenha o nome do usuário
-            message.append("Email: ").append(pedido.getUsuario().getEmail()).append("\n");
-            message.append("Telefone: ").append(pedido.getUsuario().getTelefone()).append("\n");
-            message.append("Itens: ").append("\n\n");
-            message.append("Quantidade: ").append(pedido.getItems().stream().map(ItemPedido::getQuantidade).collect(Collectors.toSet())).append('\n');
-            message.append("Preço: ").append(pedido.getItems().stream().map(ItemPedido::getPreco).collect(Collectors.toSet())).append('\n');
-            message.append("Produto: ").append("\n\n");
-            message.append("Id do produto: ").append(pedido.getItems().stream().map(ItemPedido::getProduto).map(Produto::getId).collect(Collectors.toSet())).append('\n');
-            message.append("Nome do produto: ").append(pedido.getItems().stream().map(ItemPedido::getProduto).map(Produto::getName).collect(Collectors.toSet())).append('\n');
-            message.append("Descrição do produto: ").append(pedido.getItems().stream().map(ItemPedido::getProduto).map(Produto::getDescricao).collect(Collectors.toSet())).append('\n');
-            message.append("Preço do produto: ").append(pedido.getItems().stream().map(ItemPedido::getProduto).map(Produto::getPrice).collect(Collectors.toSet())).append("\n\n");
-            message.append("Categoria do produto: ").append('\n');
-            message.append("ID da categoria: ").append(pedido.getItems().stream().map(ItemPedido::getProduto).flatMap(produto -> produto.getCategorias().stream()).map(Categoria::getId).collect(Collectors.toSet()));
-            message.append("Nome da categoria: ").append(pedido.getItems().stream().map(ItemPedido::getProduto).flatMap(produto -> produto.getCategorias().stream()).map(Categoria::getNome).collect(Collectors.toSet()));
-            message.append("\n");
-            message.append("\n");
-            message.append("Pagamento: ").append("\n");
-            message.append("Preço total: ").append(pedido.getTotal());
-            message.append("\n");
-            message.append("Momento do pagamento: ").append(pedido.getPagamento().getMomento()).append("\n");
+            StringBuilder message = new StringBuilder("Lista de Pedidos:\n");
 
+            for (Pedido pedido : pedidos) {
+                appendPedidoDetails(message, pedido);
+            }
+
+            emailService.sendEmail(email, subject, message.toString());
+
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            // Lidar com exceções ao enviar o e-mail
+            e.printStackTrace(); // Registre a exceção ou trate de acordo com suas necessidades
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+
+    private void appendPedidoDetails(StringBuilder message, Pedido pedido) {
+        message.append("-----------------------------------------------------\n");
+        message.append("ID do Pedido: ").append(pedido.getId()).append("\n");
+        message.append("Data do Pedido: ").append(pedido.getInstante()).append("\n");
+        message.append("Status do pedido: ").append(pedido.getPedidoStatus()).append("\n");
+        message.append("Id do usuario: ").append(pedido.getUsuario().getId()).append("\n");
+        message.append("Nome do Usuário: ").append(pedido.getUsuario().getNome()).append(" ").append(pedido.getUsuario().getSobrenome()).append("\n");
+        message.append("Email: ").append(pedido.getUsuario().getEmail()).append("\n");
+        message.append("Telefone: ").append(pedido.getUsuario().getTelefone()).append("\n");
+        message.append("Itens:\n");
+
+        for (ItemPedido item : pedido.getItems()) {
+            appendItemDetails(message, item);
         }
 
-        emailService.sendEmail(email, subject, message.toString());
-        return ResponseEntity.noContent().build();
+        message.append("Preço total: ").append(pedido.getTotal()).append("\n");
+        message.append("Momento do pagamento: ").append(pedido.getPagamento().getMomento()).append("\n");
     }
+
+    private void appendItemDetails(StringBuilder message, ItemPedido item) {
+        message.append("Quantidade: ").append(item.getQuantidade()).append("\n");
+        message.append("Preço: ").append(item.getPreco()).append("\n");
+        message.append("Produto:\n");
+        message.append("Id do produto: ").append(item.getProduto().getId()).append("\n");
+        message.append("Nome do produto: ").append(item.getProduto().getName()).append("\n");
+        message.append("Descrição do produto: ").append(item.getProduto().getDescricao()).append("\n");
+        message.append("Preço do produto: ").append(item.getProduto().getPrice()).append("\n");
+        message.append("Categoria do produto:\n");
+
+        for (Categoria categoria : item.getProduto().getCategorias()) {
+            appendCategoriaDetails(message, categoria);
+        }
+    }
+
+    private void appendCategoriaDetails(StringBuilder message, Categoria categoria) {
+        message.append("ID da categoria: ").append(categoria.getId()).append("\n");
+        message.append("Nome da categoria: ").append(categoria.getNome()).append("\n");
+    }
+
 
 
 
