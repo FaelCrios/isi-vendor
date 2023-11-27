8cb4e3963b84e40674f863439969bd898868f761
Rafael <faelnek@gmail.com>
2023-11-27 20:34:34 -0300

    Adding - docker to project

--- src/main/java/com/isi/isivendor/exceptions/handler/CustomizedResponseEntities.java
+++ src/main/java/com/isi/isivendor/exceptions/handler/CustomizedResponseEntities.java
@@ -1,13 +1,10 @@
 package com.isi.isivendor.exceptions.handler;
 
 import com.isi.isivendor.exceptions.ExceptionResponse;
-import com.isi.isivendor.exceptions.InvalidJwtAuthenticationException;
 import com.isi.isivendor.exceptions.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -43,15 +40,5 @@ public class CustomizedResponseEntities extends ResponseEntityExceptionHandler {
     }
 
 
-
-    @ExceptionHandler(InvalidJwtAuthenticationException.class)
-    public final ResponseEntity<ExceptionResponse> handleInvalidJwtAuthenticationException(Exception ex, WebRequest request){
-        ExceptionResponse exceptionResponse = new ExceptionResponse(
-                new Date(),
-                ex.getMessage(),
-                request.getDescription(false));
-
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
-    }
 
 }
